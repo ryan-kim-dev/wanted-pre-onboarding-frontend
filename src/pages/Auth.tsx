57431274e8923b970671e7d528c284c2a1343620
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-12-06 18:21:23 +0900

    docs: 과제 요구사항 주석 첨부

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -3,6 +3,21 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { ROUTEPATH } from '../constants';
 
+/**
+ Assignment2
+
+로그인 API를 호출하고, 올바른 응답을 받았을 때 /todo 경로로 이동해주세요
+로그인 API는 로그인이 성공했을 시 Response Body에 JWT를 포함해서 응답합니다.
+응답받은 JWT는 로컬 스토리지에 저장해주세요
+
+Assignment3
+
+로그인 여부에 따른 리다이렉트 처리를 구현해주세요
+로컬 스토리지에 토큰이 있는 상태로 / 페이지에 접속한다면 /todo 경로로 리다이렉트 시켜주세요
+로컬 스토리지에 토큰이 없는 상태로 /todo페이지에 접속한다면 / 경로로 리다이렉트 시켜주세요
+
+*/
+
 function Auth() {
   const navigate = useNavigate();
   const [formType, setFormType] = useState('signup');
